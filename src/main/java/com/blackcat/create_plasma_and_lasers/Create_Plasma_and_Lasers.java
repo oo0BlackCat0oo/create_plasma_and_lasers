3c64330a434eda7f2e8136752e5b982f0e32a1dc
oo0BlackCat0oo <118299087+oo0BlackCat0oo@users.noreply.github.com>
2023-07-12 19:55:39 +0200

    The things

---
+++ src/main/java/com/blackcat/create_plasma_and_lasers/Create_Plasma_and_Lasers.java
@@ -0,0 +1,41 @@
+package com.blackcat.create_plasma_and_lasers;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Create_Plasma_and_Lasers.MOD_ID)
+public class Create_Plasma_and_Lasers
+{ public static final String MOD_ID = "cpal";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public Create_Plasma_and_Lasers()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event){
+
+    }
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){
+
+        }
+    }
+}
+
